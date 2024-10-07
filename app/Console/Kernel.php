43ab84109e06e8a316c5177eb227adb2c9298581
Marcos <marcosvazquez99@gmail.com>
2024-10-07 18:12:33 +0200

    + Implement simple reports

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 
-        $schedule->command('time-tracking:send-report 0 "Reporte Diario" "simple" "C07R3NTSV09"')->weekdays()->at('16:13');
+        $schedule->command('time-tracking:send-report 0 "Reporte Diario" "simple" "C07R3NTSV09"')->weekdays()->at('18:00');
         $schedule->command('time-tracking:send-report 0 "Reporte Diario" "completo"')->weekdays()->at('18:00');
         $schedule->command('time-tracking:send-report 5 "Reporte Semanal" ')->fridays()->at('18:00');
         $schedule->command('time-tracking:send-report 30 "Reporte mensual" ')->monthlyOn('30', '18:00');
