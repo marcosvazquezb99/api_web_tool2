b2238e9481ae3df4a9c109d9d80f6304685a5cb1
Marcos <marcosvazquez99@gmail.com>
2024-11-04 21:15:49 +0100

    + Webhook new forms

--- app/Http/Controllers/FormsController.php
+++ app/Http/Controllers/FormsController.php
@@ -9,8 +9,13 @@ class FormsController extends Controller
     //Create a request that response the same data that the form sends
     public function formCreation(Request $request)
     {
+
+        //if the body has a event key, then send it to the slack channel
+        if ($request->has('event')) {
+            $slackController = new SlackController();
+            $response = $slackController->chat_post_message('C07PF06HF46', json_encode($request['event']));
+        }
         //log the request
-        \Log::info($request->all());
         return response()->json($request->all());
     }
 }
